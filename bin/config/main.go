a2ccd7180160c81321f853d8f9cf5466995863ec
Twelfthonehone <honehone.41games@gmail.com>
2025-11-16 17:52:43 +0900

    added config generator

---
+++ bin/config/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"ouchi/ttlcache"
+)
+
+func main() {
+	config := ttlcache.ConfigFile{
+		Headers: map[string]string{
+			"Cache-Control": "max-age=900",
+		},
+		TtlSec:     900,
+		TickSec:    60,
+		OriginPort: 8083,
+		ListenPort: 8084,
+	}
+
+	b, err := json.MarshalIndent(config, "", "  ")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := os.WriteFile("config.json", b, 0660); err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("done")
+}
